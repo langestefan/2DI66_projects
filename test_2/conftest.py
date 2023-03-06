ca00e894bbd857115f3bc5a6a2c940a44826153f
Bi-rong Wang <birongwang01@gmail.com>
2023-03-06 17:18:09 +0100

    Added tests for assignment 2

---
+++ test_2/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+import sys
+import os
+
+# to enable parent directory imports
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+# Mark the test root directory
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# Context to retain objects passed between steps
+class Context:
+    foo = None
+
+
+@pytest.fixture()
+def foo_bar():
+    return None
---
+++ test_2/test_simulator.py
@@ -0,0 +1,39 @@
+import pytest
+from scipy import stats
+
+from assignment_2.simulation import QueueSimulator
+from assignment_2.dist.Distribution import Distribution
+
+
+class TestSimulator:
+    @pytest.fixture(autouse=True)
+    def create_simulator(self):
+        """
+        Creates a game state object.
+        """
+        distributions = [Distribution(stats.poisson(1))]
+        distributions.append(Distribution(stats.expon(scale=1/80)))
+        distributions.append(Distribution(stats.geom(1/3)))
+        distributions.append(Distribution(stats.bernoulli(0.4)))
+        distributions.append(Distribution(stats.expon(scale=1/20)))
+        distributions.append(Distribution(stats.expon(scale=1/12)))
+        
+        return QueueSimulator(
+            distributions, 1, 3, False, False, 1 
+        )
+            
+
+    @pytest.fixture(autouse=True)
+    def test_simulator_init(self, create_simulator):
+        """
+        Tests the initialization of the simulator.
+        """
+        assert create_simulator.sim_history is not None
+        assert create_simulator.parallelize is False
+
+    def test_simulator_run(self, create_simulator):
+        """
+        Tests the run method of the simulator.
+        """
+        create_simulator.run(n=1)
+        assert create_simulator.sim_history.get_number_of_simulations() == 1
